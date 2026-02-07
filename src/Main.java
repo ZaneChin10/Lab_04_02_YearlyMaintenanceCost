e7d81eb1e5b50e93d1e76ca5fa5da32eaf768cf9
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-02-07 12:08:15 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        double winterCosts = 100.50;
+        double springCosts = 150.25;
+        double summerCosts = 200.75;
+        double fallCosts = 250.0;
+        double totalCosts = winterCosts + springCosts + summerCosts + fallCosts;
+
+        System.out.println("The cost for Winter is: " + winterCosts);
+        System.out.println("The cost for Spring is: " + springCosts);
+        System.out.println("The cost for Summer is: " + summerCosts);
+        System.out.println("The cost for Fall is: " + fallCosts);
+        System.out.println("The total cost for the season is: " + totalCosts);
+
+    }
+
+}
