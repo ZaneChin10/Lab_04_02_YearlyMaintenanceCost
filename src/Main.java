b9e968fce948d138ac4b40ae162e58f1b7735fd8
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-02-07 12:09:13 -0500

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
         double fallCosts = 250.0;
         double totalCosts = winterCosts + springCosts + summerCosts + fallCosts;
 
-        System.out.println("The cost for Winter is: " + winterCosts);
-        System.out.println("The cost for Spring is: " + springCosts);
-        System.out.println("The cost for Summer is: " + summerCosts);
-        System.out.println("The cost for Fall is: " + fallCosts);
-        System.out.println("The total cost for the season is: " + totalCosts);
+        System.out.println("The maintenance cost for Winter is: " + winterCosts);
+        System.out.println("The maintenance cost for Spring is: " + springCosts);
+        System.out.println("The maintenance cost for Summer is: " + summerCosts);
+        System.out.println("The maintenance cost for Fall is: " + fallCosts);
+        System.out.println("The total maintenance cost for the season is: " + totalCosts);
 
     }
 
